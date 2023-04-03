ac4d63153b8fcc96ac33a18e91579a3e7e5249ec
Viennarz Curtiz <viennarzc@gmail.com>
2023-04-03 21:11:35 +0800

    Initial Commit

---
+++ HWS-NoteDictate Watch App/HWS_NoteDictateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_NoteDictateApp.swift
+//  HWS-NoteDictate Watch App
+//
+//  Created by Viennarz Curtiz on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HWS_NoteDictate_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
